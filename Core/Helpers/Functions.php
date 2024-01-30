48153a2d85a8c5fa675b496ce3f2166bf27c99a3
Rar Zar <yarzarminkhant2003@gmail.com>
2024-01-30 09:47:30 +0630

    create logger system

--- Core/Helpers/Functions.php
+++ Core/Helpers/Functions.php
@@ -4,3 +4,13 @@ function dd($data){
     echo "<pre>";
     die(var_dump($data));
 }
+
+function logger($message)
+{
+    $timestamp = date('Y-m-d H:i:s');
+    $logFile = "app.log";
+    $logLine = "[$timestamp] : $message";
+
+    // Append log message to the log file
+    file_put_contents($logFile, $logLine, FILE_APPEND);
+}
